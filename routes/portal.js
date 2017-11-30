dca40dc45ec730ea8ec88007b4bf56019b5799e5
Steve Johnson <s.johnson@cablelabs.com>
2017-11-30 14:26:02 -0700

    set content type to JSON on registration-token

--- routes/portal.js
+++ routes/portal.js
@@ -9,19 +9,11 @@ const express = require('express');
 const router = express.Router();
 const registration = require('../lib/registration.js');
 
-/* GET registration token. */
-router.get('/registration-token/:deviceID', function(req, res, next) {
-	console.log(JSON.stringify(req.headers));
-    let token = {}
-    token.registration = {"token": registration.begin(req.params.deviceID,"clientxxx")}
-    res.send(JSON.stringify(token, null, 2)+"\n");
-});
-
-// New, use post, deviceID and clientID in body
 /* POST registration token. */
 router.post('/registration-token', function(req, res, next) {
     let token = {}
     token.registration = {"token": registration.begin(req.body)}
+    res.setHeader('content-type', 'application/json');
     res.send(JSON.stringify(token, null, 2)+"\n");
 });
 
